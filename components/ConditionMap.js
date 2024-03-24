df34f3737c65a2edf595820c6e46e699ad50b7a8
Annie <yanannie@students.cs.ubc.ca>
2024-03-23 19:13:52 -0700

    added code to other conditionmap.js

--- components/ConditionMap.js
+++ components/ConditionMap.js
@@ -1,5 +1,5 @@
-// import * as d3 from 'd3';
-// import { useEffect } from 'react';
+import * as d3 from 'd3';
+import { useEffect } from 'react';
 
 const ConditionMap = ({ data, id }) => {
   useEffect(() => {
