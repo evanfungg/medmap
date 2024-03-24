150f71f71734fb5a487b16c947780eaaa7db87de
DannyPirouz <dannypirouz@gmail.com>
2024-03-23 23:02:24 -0700

    made ui look nicer

--- app/med-search/page.js
+++ app/med-search/page.js
@@ -44,6 +44,7 @@
 //     );
 // }
 import React, { useState } from 'react';
+import './style.css';
 
 export default function Home() {
     const [searchQuery, setSearchQuery] = useState('');
@@ -80,29 +81,54 @@ export default function Home() {
         setClientSearchResult(clientSearchData.text);
     };
 
+    // const handleWebSearchInputKeyDown = (event) => {
+    //     if (event.key === 'Enter') {
+    //         handleWebSearchSubmit();
+    //     }
+    // };
+
+    // const handleClientSearchInputKeyDown = (event) => {
+    //     if (event.key === 'Enter') {
+    //         handleClientSearchSubmit();
+    //     }
+    // };
+
     return (
         <main className="main">
+            <h1 className="title">Medication Search</h1> 
             <form className="form-container" onSubmit={handleWebSearchSubmit}>
                 <input
                     type="text"
                     placeholder="Enter Generic (Scientific) Name of Medication"
                     onChange={(e) => setSearchQuery(e.target.value)}
+                    // onKeyDown={handleWebSearchInputKeyDown}
                     value={searchQuery}
                 />
                 <button className="submit" type="submit">Search</button>
             </form>
-            {searchResult}
+            <div className="search-result">{searchResult}</div> 
+            {/* {searchResult} */}
 
             <form className="form-container" onSubmit={handleClientSearchSubmit}>
                 <input
                     type="text"
                     placeholder="Enter Brand Name of Medication"
                     onChange={(e) => setClientSearchQuery(e.target.value)}
+                    // onKeyDown={handleClientSearchInputKeyDown}
                     value={clientSearchQuery}
                 />
                 <button className="submit" type="submit">Search</button>
             </form>
-            {clientSearchResult}
+            <div className="search-result">{clientSearchResult}</div>
+            <div className="image-container">
+                <img src="https://www.duffystric.com/var/images/product/500.500/D/4614-01.jpg" alt="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Pill_1.jpg/1200px-Pill_1.jpg" className="image" id="advil"/>
+                <img src="https://symbols.getvecta.com/stencil_195/15_right-arrow.a0745525dc.svg" alt="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Pill_1.jpg/1200px-Pill_1.jpg" className="image" id="arrow"/>
+                
+                <img src="https://www.researchgate.net/profile/Murad-Abualhasan/publication/270283844/figure/fig1/AS:613968762642434@1523392968418/Chemical-structure-of-Ibuprofen.png" alt="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Pill_1.jpg/1200px-Pill_1.jpg" className="image" id="ibu"/>
+            </div>
+            {/* <img className="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Pill_1.jpg/1200px-Pill_1.jpg" alt="" />
+            <img className="image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Pill_1.jpg/1200px-Pill_1.jpg" alt="" /> */}
+            {/* {clientSearchResult} */}
         </main>
     );
 }
