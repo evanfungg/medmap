e73e89446dbec7ed654da1320bb66c6153645762
Your Name <you@example.com>
2024-03-23 20:42:07 -0700

    UI

--- app/components/NavButton.js
+++ app/components/NavButton.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import Link from "next/link"
 
 
-const NavButton = ({link, name}) => {
+import React from 'react';
+import Link from "next/link";
+import './styles.css'
+
+
+
+const NavButton = ({ link, name }) => {
   return (
-    <Link href = {link}><button>{name}</button></Link>
-  )
+    <Link href={link}>
+      <button className = "btn">{name}</button>
+    </Link>
+  );
+};
 
-}
-
-export default NavButton
+export default NavButton;
--- components/NavButton.js
+++ components/NavButton.js
@@ -1,11 +1,31 @@
-import React from 'react'
-import Link from "next/link"
+// // NavButton.js
 
+// import React from 'react';
+// import Link from "next/link";
+// import styled from "styled-components";
 
-const NavButton = ({link, name}) => {
-  return (
-    <Link href = {link}><button>{name}</button></Link>
-  )
-}
+// // Define StyledButton outside of the NavButton component
+// const StyledButton = styled.button`
+//   flex-grow: 1; /* This will allow each button to grow and fill the space */
+//   background-color: #007bff;
+//   color: white;
+//   padding: 10px 20px;
+//   border: none;
+//   cursor: pointer;
+//   transition: background-color 0.3s;
+//   &:hover {
+//     background-color: #0056b3;
+//   }
+//   /* Ensure text is centered */
+//   text-align: center;
+// `;
 
-export default NavButton
+// const NavButton = ({ link, name }) => {
+//   return (
+//     <Link href={link}>
+//       <StyledButton>{name}</StyledButton>
+//     </Link>
+//   );
+// };
+
+// export default NavButton;
